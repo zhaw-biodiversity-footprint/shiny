3bb94b976d2ff3240f75bc23f12171ad6642ed40
Evelyn Terzer <terz@zhaw.ch>
2025-01-23 22:33:51 +0100

    style changes

--- ui.R
+++ ui.R
@@ -3,69 +3,54 @@ source("utils.R")
 
 
 ### UI
-page_navbar(
-    nav_panel("Projekt",
-      fluidRow(
-        layout_columns(
-          max_height = 75,
-          width = 25,
-          imageOutput("image1"),
-          imageOutput("image2"),
-          imageOutput("image3")
-        )
-      ),
-      fluidRow(includeHTML("project_information.html")
-      )
-    ),
-    nav_panel("Warenkorb",
-      layout_column_wrap(
-        width = NULL,
-        style = css(grid_template_columns = "1fr 2fr"),
-          card(fill = F,
-               min_height = 500,
-               card_header("Warenkorb"),
-               card_body(
-                 max_height = 200,
-                 value_box(
-                   title = "Totale Summe gewählter Warenkorb (PDF)",
-                   showcase = icon("shopping-cart"),
-                   value = uiOutput("pdf_total"),
-                   theme = "bg-gradient-blue-purple")
-               ),
-               numericInput("n", "Anzahl Produkte", value = 1, min = 1, max=10),
-               card_body(
-                 min_height = 700,
-                 layout_column_wrap(
-                   width = 1/2,
-                   card("Produkte",
-                        uiOutput("product")),
-                   card("Menge (kg)",
-                        uiOutput("weight"))
-                  )
-                ),
-           ),
-          card(height = 800,
-               card_header("PDF des Warenkorbes"),
-               card_body(
-                 layout_column_wrap(
-                   min_height = 500,
-                   width = 1/2,
-                   leaflet::leafletOutput("map", width = 1000),
-                   plotlyOutput("barchart_ch")
-                 )
-               ),
-               card_body(
-                 min_height = 200,
-                 layout_column_wrap(
-                   width = 1/2,
-                   plotlyOutput("sunburst"),
-                   plotlyOutput("piechart_warenkorb")
-                  )
+page_fluid(
+  includeCSS("bootstrap.css"),
+    layout_column_wrap(
+      width = NULL,
+      style = css(grid_template_columns = "1fr 2fr"),
+        card(fill = F,
+             min_height = 500,
+             card_header("Warenkorb"),
+             card_body(
+               max_height = 200,
+               value_box(
+                 title = "Totale Summe gewählter Warenkorb (PDF)",
+                 showcase = icon("shopping-cart"),
+                 value = uiOutput("pdf_total"),
+                 theme = value_box_theme(bg = "#2c3e50", fg = "#f8f9fa"),
+                 class = "border")
+             ),
+             numericInput("n", "Anzahl Produkte", value = 1, min = 1, max=10),
+             card_body(
+               min_height = 700,
+               layout_column_wrap(
+                 width = 1/2,
+                 card("Produkte",
+                      uiOutput("product")),
+                 card("Menge (kg)",
+                      uiOutput("weight"))
                 )
-          )
-      )
-    ),
-    title =  "Shop Hero",
-    id="page"
+              ),
+         ),
+        card(height = 800,
+             card_header("PDF des Warenkorbes"),
+             card_body(
+               layout_column_wrap(
+                 min_height = 500,
+                 width = 1/2,
+                 leaflet::leafletOutput("map", width = 1000),
+                 plotlyOutput("barchart_ch")
+               )
+             ),
+             card_body(
+               min_height = 200,
+               layout_column_wrap(
+                 width = 1/2,
+                 plotlyOutput("sunburst"),
+                 plotlyOutput("piechart_warenkorb")
+                )
+              )
+        )
+    )
 )
 
