924bf9e27e92f5c0f3837c4df9004c10582a4a37
Evelyn Terzer <terz@zhaw.ch>
2025-01-28 20:39:35 +0100

    add action button and bug fixing

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 ### Server-Funktion
 
 function(input, output, session) {
-
+  
   ### Basket
   
   #create list with product number depending on how many products can be selected
@@ -15,64 +15,45 @@ function(input, output, session) {
   
   #create as many sliders for weight as number selected
   output$weight <- renderUI({
-    # if(length(pdfs_products())>2){
-    #   browser()
-    #   if(is_null(map(weights_input(), ~input[[.x]])[[1]])){
-        map(weights_input(), ~ numericInput(.x, NULL,  value = 1, min = 0.1, max = 100, step = 0.1))
-      # }else{
-      #   browser()
-      #   map(weights_input(), ~ sliderInput(.x, NULL,  min = 1, max = 10, value = as.numeric(input[[.x]])))
-      # }
-    # }
+        map(weights_input(), ~ numericInput(.x, NULL,  value = as.numeric(input[[.x]]), min = 0.1, max = 100, step = 0.1))
   })
+  
   
   ### Calculate data (preperation)
   
   #get pdfs of the selected products
-  pdfs_products <- reactive({map(product_input(), ~ dat_uebersicht2$PDF_kg[dat_uebersicht2$Produkt==input[[.x]]])})
+  pdfs_products <- eventReactive(input$display,{map(product_input(), ~ dat_uebersicht2$PDF_kg[dat_uebersicht2$Produkt==input[[.x]]])})
 
   #fill pdfs where no products is selected with 0
-  pdf_products_full <- reactive(rapply(pdfs_products(),function(x) ifelse(identical(x, numeric(0)),0,x), how = "replace"))
+  pdf_products_full <- eventReactive(input$display,{rapply(pdfs_products(),function(x) ifelse(identical(x, numeric(0)),0,x), how = "replace")})
   
   #get imput weights as vector
-  weights <- reactive(unlist(map(weights_input(), ~input[[.x]])))
+  
+  weights <- eventReactive(input$display,{map(weights_input(), ~input[[.x]])})
+  
+  #if no weight selected NA to 0
+  weights_full_vector <- eventReactive(input$display, {replace_na(unlist(weights()), 0)})
+  
   
   #calculate pdfs of products with weight --> impacts as list
-  impacts <- reactive({
-    # if(length(pdfs_products())>2){
+  impacts <-eventReactive(input$display,{
+    # if(length(pdfs_products())>0){
     #   browser()
     # }
-    # as.list(unlist(pdf_products_full()) * unlist(map(weights_input(), ~input[[.x]]))[seq_along(unlist(pdf_products_full()))])
-    as.list(unlist(pdf_products_full()) * unlist(map(weights_input(), ~input[[.x]]))) #gibt warnung
+    as.list(unlist(pdf_products_full()) *   weights_full_vector()) #gibt warnung
     })
   
-  # have impacts per selected product as vector
-  impacts_vector <- reactive({unlist(impacts())})
-
-  #render impacts values as text (not necessary anymore, not visible in UI)
-  # output$pdf <- renderUI({
-  #   # if(length(pdfs_products())>2){
-  #   #   browser()
-  #   # }
-  #   map(impacts(), ~ renderText(.x))
-  # })
-  
   # get names of products as list
-  pdfs_names <- reactive({map(product_input(), ~ input[[.x]])})
-  
-  # have names of products as vector
-  pdfs_names_vector <- reactive({unlist(pdfs_names())})
+  pdfs_names_vector <- eventReactive(input$display,{unlist(map(product_input(), ~ input[[.x]]))})
   
   
   # filter dat with pdf per country, only selected products in basket
   
-  dat_selected <- reactive({
-    if(sum(unlist(impacts()))>0){
+  dat_selected <- eventReactive(input$display,{
       dat |> 
         filter(Produkt %in% pdfs_names_vector()) |> 
         filter(PDF_CH_mean > 0) |> 
         mutate(impact = 0)
-    }
   })
   
   # calculate impacts per selected product and country
@@ -84,11 +65,21 @@ function(input, output, session) {
     return(data)
   }
   
-  dat_selected2 <- reactive({calculate_impact(dat_selected(), pdfs_names_vector(), weights())})
+  dat_selected2 <- eventReactive(input$display,{
+      calculate_impact(dat_selected(), pdfs_names_vector(), weights_full_vector())
+  })
+  
+  
+  ### Show only outputs when this condition is met (product and weight are selected)
+  condition <- reactive(sum(!is.na(weights())) == sum(pdfs_names_vector()!=""))
   
   
   ### Total sum of basket
-  output$pdf_total <- renderText({formatC(sum(unlist(impacts())), format = "e", digits = 2)})
+  observeEvent(input$display,{
+    if(condition()){
+      output$pdf_total <- renderText({formatC(sum(unlist(impacts()), na.rm = T), format = "e", digits = 2)})
+    }
+  })
   
   
   ### Pie chart: percent of each product in selected basket
@@ -97,38 +88,36 @@ function(input, output, session) {
   
   # sum of impact for each product 
   
-  dat_selected_sum_prod <- reactive({
-    if(sum(unlist(impacts()))>0){
+  dat_selected_sum_prod <- eventReactive(input$display,{
       dat_selected2() |>
         group_by(Produkt) |>
         summarise(impact_product = sum(impact))
-    }
   })
-  # 
-  # # get per product the highest three impacts of countries 
+  #
+  # # get per product the highest three impacts of countries
   # calculate_highest_countries <- function(data, data_sum){
   #   data_sum <- mutate(data_sum, top_country = 0)
   #   for(i in 1:nrow(data_sum)){
-  #     top_country_n3 <- data |> 
-  #       filter(Produkt==data_sum$Produkt[i]) |> 
-  #       top_n(3, impact) |> 
+  #     top_country_n3 <- data |>
+  #       filter(Produkt==data_sum$Produkt[i]) |>
+  #       top_n(3, impact) |>
   #       arrange(desc(impact))
-  #     
+  #
   #     data_sum$top_country[i] <- paste(top_country_n3$Land, top_country_n3$impact, sep = ": ", collapse = ", ")
   #   }
   #   return(data_sum)
   # }
-  # 
+  #
   # piechart_df <- reactive({calculate_highest_countries(dat_selected2(), dat_selected_sum_prod())})
-  # 
+  #
   # # plot only impact > 0
   # piechart_df_filter <- reactive({
   #   if(sum(unlist(impacts()))>0){
   #     subset(piechart_df(), impact_product != "0")}
   #     })
-  
+
   ## pie chart (not needed anymore)
-  
+
   # observe({
   #   if(sum(unlist(impacts()))>0){
   #     output$piechart_warenkorb <- renderPlotly({
@@ -142,70 +131,71 @@ function(input, output, session) {
   #   }
   # })
 
-  
+
   ### Sunburst
-  
-  dat_selected3 <- reactive({
-    dat_selected2() |>
-      mutate(Land_Produkt = paste(dat_selected2()$Produkt, dat_selected2()$Land, sep = "_"))
-  })
-  
-  dat_top3 <- reactive({
-    dat_selected3() |>
-      select(c(1,2,7,8)) |> 
-      group_by(Produkt) |> 
-      top_n(3, impact)
+
+  dat_selected3 <- eventReactive(input$display,{
+
+      dat_selected2() |>
+        mutate(Land_Produkt = paste(dat_selected2()$Produkt, dat_selected2()$Land, sep = "_"))
+
   })
 
-  dat_rest <- reactive({
-    dat_selected3() |> 
-      filter(!(Land_Produkt %in% dat_top3()$Land_Produkt)) |> 
-      group_by(Produkt) |> 
-      summarise(impact = sum(impact)) |> 
+  dat_top3 <- eventReactive(input$display,{
+      dat_selected3() |>
+        select(c(1,2,7,8)) |>
+        group_by(Produkt) |>
+        top_n(3, impact)
+  })
+
+  dat_rest <- eventReactive(input$display,{
+    dat_selected3() |>
+      filter(!(Land_Produkt %in% dat_top3()$Land_Produkt)) |>
+      group_by(Produkt) |>
+      summarise(impact = sum(impact)) |>
       mutate(Land = "Übrige")
   })
-  
-  dat_all <- reactive({
-    rbind(select(dat_top3(),-4),dat_rest()) |> 
+
+  dat_all <- eventReactive(input$display,{
+    rbind(select(dat_top3(),-4),dat_rest()) |>
       arrange(Produkt)
   })
-  
+
   # df_sunburst <- reactive({
   #   if(sum(unlist(impacts()))>0){
   #     bind_rows(
   #       # Full total
   #       dat_all() |> summarise(labels = "Total", impact = sum(impact)),
   #       # "Class"-level totals
-  #       dat_all() |> 
-  #         group_by(labels = Produkt) |>  
+  #       dat_all() |>
+  #         group_by(labels = Produkt) |>
   #         summarise(
-  #           impact = sum(impact), 
+  #           impact = sum(impact),
   #           parents = "Total",
   #           .groups = "drop"),
   #       # Individual Class+Survived-level numbers
-  #       dat_all() |> 
-  #         rename(parents = Produkt, labels = Land) |> 
-  #         mutate(parents = paste("Total", parents, sep = " - "))) |> 
+  #       dat_all() |>
+  #         rename(parents = Produkt, labels = Land) |>
+  #         mutate(parents = paste("Total", parents, sep = " - "))) |>
   #       # Add unique ids column
   #       mutate(ids = if_else(
   #         is.na(parents), labels, paste(parents, labels, sep = " - ")))
   #   }
   # })
-  
-  df_sunburst <- reactive({
-    if(sum(unlist(impacts()))>0){
-     as.sunburstDF(dat_all(), value_column = "impact", add_root = T)
-    }
+
+  df_sunburst <- eventReactive(input$display,{
+     as.sunburstDF(dat_all(), value_column = "impact", add_root = T) |> 
+      filter(values > 0)
   })
 
-  observe({
-    if(sum(unlist(impacts()))>0){
+  observeEvent(input$display,{
+    if(condition()){
       output$sunburst <- renderPlotly({
         fig <- df_sunburst() |>
-          plot_ly(ids = ~ids, labels = ~labels, parents = ~parents, 
+          plot_ly(ids = ~ids, labels = ~labels, parents = ~parents,
                   values = ~values, type = "sunburst", branchvalues = "total",
                   hoverinfo ="text",
-                  hovertext = ~paste(labels, "<br>", "PDF: ",formatC(values, format ="e", digits = digits))) |> 
+                  hovertext = ~paste(labels, "<br>", "PDF: ",formatC(values, format ="e", digits = digits))) |>
           layout(title = list(text = "Biodiversitäts-Fussabdruck ausgewählter Produkt",
                               x = 0.5,
                               xanchor = "center"),
@@ -225,33 +215,34 @@ function(input, output, session) {
       })
     }
   })
-  
+
 
   ### Bar chart
-  
-  dat_bar <- reactive({dat_selected_sum_prod() |> 
+
+  dat_bar <- eventReactive(input$display,{dat_selected_sum_prod() |>
       mutate(Quelle = "Selektierter Warenkorb")})
-  
-  dat_bar_important_prod <- reactive({dat_bar() |> 
+
+  dat_bar_important_prod <- eventReactive(input$display,{dat_bar() |>
       filter(Produkt %in% important_products)})
 
-  dat_bar_uebrige <- reactive({dat_bar() |> 
+  dat_bar_uebrige <- eventReactive(input$display,{dat_bar() |>
       filter(!(Produkt %in% important_products)) |>
       summarise(impact_product = sum(impact_product))})
-    
-  dat_bar_selected <- reactive(rbind(dat_bar_important_prod(), data.frame(Produkt="Übrige", impact_product=dat_bar_uebrige(), Quelle="Selektierter Warenkorb")))
-  
-  dat_bar_chart2 <- reactive({
-    rbind(dat_bar_chart, dat_bar_selected()) |> 
+
+  dat_bar_selected <- eventReactive(input$display,{
+    rbind(dat_bar_important_prod(), data.frame(Produkt="Übrige", impact_product=dat_bar_uebrige(), Quelle="Selektierter Warenkorb"))})
+
+  dat_bar_chart2 <- eventReactive(input$display,{
+    rbind(dat_bar_chart, dat_bar_selected()) |>
       mutate(impact_product_percent = impact_product / ch_pdf_woche * 100)})
 
-  observe({
-    if(sum(unlist(impacts())) > 0){
+  observeEvent(input$display,{
+    if(condition()){
       output$barchart_warenkorb <- renderPlotly({
         fig <- dat_bar_chart2() |>
           plot_ly(x = ~impact_product_percent, y = ~Quelle, type = "bar", color = ~Produkt, showlegend = T,
                   hoverinfo ="text",
-                  hovertext = ~paste("Produkt: ", Produkt , "<br>", "Anteil PDF (%): ", round(impact_product_percent,2)))|> 
+                  hovertext = ~paste("Produkt: ", Produkt , "<br>", "Anteil PDF (%): ", round(impact_product_percent,2)))|>
           layout(xaxis = list(title = 'PDF', exponentformat = "e"), yaxis = list(title = '', tickangle = 270), barmode = 'stack')
       })
     }
@@ -262,7 +253,7 @@ function(input, output, session) {
   #     quelle = c("PDF CH total", "gewählter Warenkorb"),
   #     pdf_total_warenkorb = c(sum(dat_uebersicht$Total_PDF_Produkt_CH, na.rm = TRUE), sum(unlist(impacts())))
   #   )
-  # 
+  #
   #percent of PDF (selected products and weight) compared to mean CH (muss noch angepasst werden)
   # observe({
   #   if(sum(unlist(impacts())) > 0){
@@ -276,45 +267,42 @@ function(input, output, session) {
   #   }
   # })
 
-  
+
   ### Map
-  
+
   ## create dataframe for map
-  
-  # sum of impact for each country 
-  dat_selected_sum_country <- reactive({
-    if(sum(unlist(impacts()))>0){
-      dat_selected2() |> 
-        group_by(Land, Code_A3) |> 
-        summarise(impact_country = sum(impact), .groups = "keep")
-    }
+
+  # sum of impact for each country
+  dat_selected_sum_country <- eventReactive(input$display,{
+      dat_selected2() |>
+        group_by(Land, Code_A3) |>
+        summarise(impact_country = sum(impact), .groups = "keep") |> 
+        filter(impact_country>0)
   })
-  
+
   # join geometry of countries
-  dat_countries <- reactive({
-    if(sum(unlist(impacts()))>0){
+  dat_countries <- eventReactive(input$display,{
       st_as_sf(left_join(dat_selected_sum_country(), borders, join_by("Code_A3"=="GID_0")))
-    }
   })
 
   ## basemap
-  
+
   output$map <- renderLeaflet({
     leaflet() |>
       addProviderTiles(providers$CartoDB.Positron) |>
-      setView(lng = 8.25, lat =  46.85, zoom = 2)
+      setView(lng = 8.25, lat =  46.85, zoom = 1)
       #flyToBounds(lng1 = 85, lat1 = -180, lng2 = -85, lat2 = 180)
   })
 
   ## map
-  
-  observe({
-    if(sum(unlist(impacts()))>0){
+
+  observeEvent(input$display,{
+    if(condition()){
       if(nrow(dat_countries())>1){ #distinguish between products only from CH from those which are imported from abroad
-        qpal <- colorQuantile(palette = "RdYlGn", n = 8, domain = dat_countries()$impact_country, reverse = T)
+        qpal <- colorQuantile(palette = "RdYlGn", n = 6, domain = dat_countries()$impact_country, reverse = T)
         qpal_col <- qpal(dat_countries()$impact_country) # hex codes
         qpal_col_legend <- unique(qpal(sort(dat_countries()$impact_country))) # hex codes
-        qpal_labs <- quantile(dat_countries()$impact_country, seq(0, 1, length.out = 9)) # depends on n from pal
+        qpal_labs <- quantile(dat_countries()$impact_country, seq(0, 1, length.out = 7)) # depends on n from pal
         qpal_labs <- formatC(qpal_labs, format = "e", digits = digits)
         qpal_labs <- paste(lag(qpal_labs), qpal_labs, sep = " - ")[-1] # first lag is NA
         leafletProxy("map", data = dat_countries(), session) |>
@@ -356,16 +344,16 @@ function(input, output, session) {
     }
   })
 
-  
+
   #   filtered_dat_countries <- reactive({filter(dat_countries, Produkt == input$product_map)})
   #   filtered_dat_countries_poly <- reactive({select(filtered_dat_countries(), PDF_kg_prod_country)})
-  #   
+  #
   # output$map <- renderLeaflet({
   #   leaflet() |>
   #     addProviderTiles(providers$CartoDB.Positron) |>
   #     setView(lng = 8.25, lat =  46.85, zoom = 2)
   # })
-  # 
+  #
   # observe({
   #   qpal <- colorQuantile(palette = "Spectral", n = 8, domain = filtered_dat_countries_poly()$PDF_kg_prod_country, reverse = T)
   #   qpal_col <- qpal(filtered_dat_countries_poly()$PDF_kg_prod_country) # hex codes
@@ -392,28 +380,26 @@ function(input, output, session) {
   #       )
   # })
 
-  
+
   ### Barchart top countries
-  
-  top_ten_worst <- reactive({
-    if(sum(unlist(impacts()))>0){
-      dat_selected_sum_country() |> 
-        ungroup() |> 
-        top_n(10, impact_country) |> 
-        arrange(impact_country)   
-    }
+
+  top_ten_worst <- eventReactive(input$display,{
+    dat_selected_sum_country() |>
+      ungroup() |>
+      top_n(10, impact_country) |>
+      arrange(impact_country)
   })
   
-  output$barchart_countries <- renderPlotly({
-    if(sum(unlist(impacts())) > 0){
-      fig <- top_ten_worst() |>
-        plot_ly(y = ~Land, x = ~impact_country, type = "bar", orientation = 'h',
-                hovertext = ~paste("PDF: ", formatC(impact_country, format ="e", digits = digits)),
-                hoverinfo = "text") |> 
-             layout(yaxis = list(categoryorder = "array", categoryarray =  top_ten_worst()$Land, title = list(text = "")),
-                    xaxis = list(title = list(text = "PDF", standoff = 20), exponentformat = "e"))
-        
+  observeEvent(input$display,{
+    if(condition()){
+      output$barchart_countries <- renderPlotly({
+        fig <- top_ten_worst() |>
+          plot_ly(y = ~Land, x = ~impact_country, type = "bar", orientation = 'h',
+                  hovertext = ~paste("PDF: ", formatC(impact_country, format ="e", digits = digits)),
+                  hoverinfo = "text") |>
+               layout(yaxis = list(categoryorder = "array", categoryarray =  top_ten_worst()$Land, title = list(text = "")),
+                      xaxis = list(title = list(text = "PDF", standoff = 20), exponentformat = "e"))
+      })
     }
-  }) 
-  
+  })
 }
